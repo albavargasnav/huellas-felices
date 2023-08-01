93b14b67e46011ad06a19e280e8f61e964cca9fb
Alba Vargas <118747197+albavargasnav@users.noreply.github.com>
2023-08-01 16:04:24 +0200

    FRONTEND: Conectando react con el frontend

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: `${process.env.REACT_APP_API_BASE_URL}/api`,
+  baseURL: `${process.env.REACT_APP_API_BASE_URL}/apiv1`,
 });
 
 const setAuthorizationHeader = token => {
