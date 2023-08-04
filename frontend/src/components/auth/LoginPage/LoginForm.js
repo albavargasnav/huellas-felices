18632101be9ab4a9d21d23e07c51f4f63dbfee62
Alba Vargas <118747197+albavargasnav@users.noreply.github.com>
2023-08-04 11:57:26 +0200

    Current code

--- frontend/src/components/auth/LoginPage/LoginForm.js
+++ frontend/src/components/auth/LoginPage/LoginForm.js
@@ -1,6 +1,7 @@
 import T from 'prop-types';
 
 import useForm from '../../../hooks/useForm';
+import './LoginForm.css';
 
 const validEmail = ({ email }) => email;
 const validPassword = ({ password }) => password;
@@ -19,24 +20,32 @@ function LoginForm({ onSubmit, isLoading }) {
   const { email, password, remember } = credentials;
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input name="email" value={email} onChange={handleChange} />
-      <input
-        type="password"
-        name="password"
-        value={password}
-        onChange={handleChange}
-      />
-      <input
-        type="checkbox"
-        name="remember"
-        checked={remember}
-        onChange={handleChange}
-      />
-      <button disabled={!validate(validEmail, validPassword, () => !isLoading)}>
-        Login
-      </button>
-    </form>
+    <div className='row'>
+      <div className='form-container'>
+        <form className='login-form' onSubmit={handleSubmit(onSubmit)}>
+          <span className='login-form-title'>Iniciar sesión</span>
+          <div className='login-form-text-info'>Correo electrónico</div>
+          <input name="email" value={email} onChange={handleChange} placeholder='Introduce tu correo electrónico' />
+          <div className='login-form-text-info'>Contraseña</div>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            placeholder='Introduce tu contraseña'
+          />
+          <input
+            type="checkbox"
+            name="remember"
+            checked={remember}
+            onChange={handleChange}
+          />
+          <button disabled={!validate(validEmail, validPassword, () => !isLoading)}>
+            Iniciar sesión
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
