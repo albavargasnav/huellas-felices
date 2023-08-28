baa2d0778480197ed7a5c588247fecc0d0a0dc7e
Cynthia <cynthiacruz1007@gmail.com>
2023-08-28 17:45:08 +0200

    pagina info adopcion terminada

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,31 +1,34 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import { AdvertPage, AdvertsPage, NewAdvertPage } from '../adverts';
 import { LoginPage, RequireAuth } from '../auth';
 import NotFoundPage from './NotFoundPage';
 import Layout from '../layout';
+import RegistrationPage from '../auth/RegistrationPage/RegistrationPage';
 import LandingPage from '../pages/LandingPage/LandingPage';
 
 function App() {
   return (
     <Routes>
       <Route
-        path="/adverts"
+        path="/huellas-felices"
         element={
           <RequireAuth>
             <Layout />
           </RequireAuth>
         }
       >
-        <Route index element={<LandingPage />} />
+        <Route index element={ <LandingPage />} />
         <Route path="new" element={<NewAdvertPage />} />
         <Route path=":advertId" element={<AdvertPage />} />
+        <Route path="info-prote" element={<InfoProtePage />} />
+        <Route path="info-adopcion" element={<InfoAdopcionPage />} />
       </Route>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/404" element={<Layout />}>
         <Route index element={<NotFoundPage />} />
       </Route>
-      <Route path="/" element={<Navigate to="/adverts" />} />
+      <Route path="/" element={<Navigate to="/huellas-felices" />} />
       <Route path="*" element={<Navigate to="/404" />} />
     </Routes>
   );
