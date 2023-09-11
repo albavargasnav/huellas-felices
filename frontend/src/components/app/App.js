87d57eeb965b99d0bd93ef88caeed5cf154349c5
Macrosura <macrosura20@gmail.com>
2023-09-11 12:07:39 +0200

    añadido página publica

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -10,32 +10,31 @@ import InfoProtePage from "../pages/pages/InfoProtePage";
 import InfoAdopcionPage from "../pages/pages/InfoAdopcionPage";
 
 
+
 function App() {
   return (
     <Routes>
       <Route
-        path="/huellas-felices"
+        path="/adverts"
         element={
           <RequireAuth>
             <Layout />
           </RequireAuth>
         }
       >
-        <Route index element={ <LandingPage />} />
+        <Route index element={<AdvertsPage />} />
         <Route path="new" element={<NewAdvertPage />} />
         <Route path=":advertId" element={<AdvertPage />} />
-        <Route path="info-prote" element={<InfoProtePage />} />
-        <Route path="info-adopcion" element={<InfoAdopcionPage />} />
       </Route>
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/registrer" element={<RegistrationPage />} />
       <Route path="/404" element={<Layout />}>
         <Route index element={<NotFoundPage />} />
       </Route>
-      <Route path="/" element={<Navigate to="/huellas-felices" />} />
+      <Route path="/" element={<Navigate to="/adverts" />} />
       <Route path="*" element={<Navigate to="/404" />} />
     </Routes>
   );
 }
+
 
 export default App;
