df6c21608be05c88eff540bf8b161c6d41fcd908
Maria Jose Serrano Alcantara <mjserrano89@gmail.com>
2023-09-18 02:46:47 +0200

    obtener perfil usuario por nombre

--- backend/routes/api/usuarios.js
+++ backend/routes/api/usuarios.js
@@ -7,6 +7,8 @@ router.post('/', UsuarioController.crearUsuario)
 
 router.get('/:usuarioId', UsuarioController.obtenerUsuario)
 
+router.get('/name/:name', UsuarioController.obtenerUsuarioPorNombre)
+
 router.put('/:usuarioId', UsuarioController.actualizarUsuario)
 
 module.exports = router
