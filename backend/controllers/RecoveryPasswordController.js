bd1d40f87a0b87dc4cdd5d5df6ce1e78199792ef
Macrosura <macrosura20@gmail.com>
2023-09-20 02:15:10 +0200

    Cambios

--- backend/controllers/RecoveryPasswordController.js
+++ backend/controllers/RecoveryPasswordController.js
@@ -3,26 +3,21 @@ const validator = require('validator')
 
 exports.verificarEmailRegistrado = async (req, res, next) => {
   try {
-    const { email } = req.body;
+    const { email } = req.body
 
     if (!validator.isEmail(req.body.email)) {
       return res.status(400).json({mensaje: 'Correo no válido'})
     }
 
-    const usuario = await Usuario.findOne({ email: email });
+    const usuario = await Usuario.findOne({ email: email })
 
     if (!usuario) {
       res.status(404).json({ statusCode: '404', message: 'Correo electrónico no registrado' })
-      return;
+      return
     }
 
     res.status(200).json({ statusCode: '200', message: 'Correo electrónico registrado' })
   } catch (err) {
-    next(err);
+    next(err)
   }
 }
-
-
-
-
-
