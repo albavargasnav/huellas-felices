4456c4fef4d83010df819556bb5df35a7fa12a60
Alba Vargas <118747197+albavargasnav@users.noreply.github.com>
2023-09-20 03:08:22 +0200

    codigo temporal

--- backend/controllers/GeneratePasswordController.js
+++ backend/controllers/GeneratePasswordController.js
@@ -1,20 +1,42 @@
 const { Usuario } = require('../models');
 const jwt = require('jsonwebtoken');
 
-exports.verifyTokenPage = (req, res) => {
-    console.log('Pasa por aqui');
-    const token = req.body.token || req.query.token || req.get('Authorization')
+// exports.verifyTokenPage = (req, res) => {
+//     const token = req.body.token || req.query.token || req.get('Authorization')
   
-    // Verificar el token
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) {
-        console.log(token)
-        console.error('Error al verificar el token:', err);
-        return res.status(400).send('Token no válido');
-      }
+//     // Verificar el token
+//     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+//       if (err) {
+//         console.log(token)
+//         console.error('Error al verificar el token:', err);
+//         return res.status(400).send('Token no válido');
+//       }
   
-      res.status(200);
-      res.json(token);
+//       res.status(200);
+//       res.json(token);
 
-    });
-  };
+//     });
+    
+// };
+
+
+exports.verifyTokenPage = async (req, res) => {
+  const token = req.body.token || req.query.token || req.get('Authorization');
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const usuarioId = decoded.usuarioId;
+  let body = req.body
+  if (body) {
+    body.password = await Usuario.hashPassword(body.password)
+  }
+  console.log(body)
+
+  await Usuario.findOneAndUpdate({name: usuarioId}, body, function (err, place) {
+    if (err) {
+      res.status(500).json({ error: err })
+    }
+  })
+  
+  res.status(201).json({ result: 'Actualizado correctamente' })
+    
+};
--- backend/routes/api/generate_password.js
+++ backend/routes/api/generate_password.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const GeneratePasswordController = require('../../controllers/GeneratePasswordController')
 
-router.post('/', GeneratePasswordController.verifyTokenPage);
+router.put('/', GeneratePasswordController.verifyTokenPage);
 
   
 module.exports = router
